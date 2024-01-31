5facffd2f31d150b90f0cf5015dd6caf96c05591
TpRaphulu <tpraphulu@gmail.com>
2024-01-31 22:47:10 +0200

    created weather forecast

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <div classNmae="container">
-        <Weather defaultCity="Toronto" />
-        <footer>
-          This project was coded by Jasmyn Garrido and is {""}
-          <a
-            href="https://github.com/Jazzi27/weather-search-engine"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on Github
-          </a>
-        </footer>
+      <div className="container">
+        <Weather defaultCity="Glasgow" />
       </div>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/Mariadoc1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Maria Docherty
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Mariadoc1/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          open-sourced
+        </a>{" "}
+        on Github. It is hosted on{" "}
+        <a
+          href="https://scintillating-baklava-70c67f.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify.
+        </a>
+      </footer>
     </div>
   );
 }
