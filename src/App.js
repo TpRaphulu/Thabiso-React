99fb9f6f6fed1ce6555bed60532d0858257e81a2
TpRaphulu <tpraphulu@gmail.com>
2024-01-31 22:34:33 +0200

    created components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div classNmae="container">
+        <Weather defaultCity="Toronto" />
+        <footer>
+          This project was coded by Jasmyn Garrido and is {""}
+          <a
+            href="https://github.com/Jazzi27/weather-search-engine"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
